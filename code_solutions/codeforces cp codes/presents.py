afd2fb0453d2eaeb976b1b73a5ece81dfa3cb88c
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-12-21 09:36:21 +0530

    adding new codefiles

---
+++ code_solutions/codeforces cp codes/presents.py
@@ -0,0 +1,34 @@
+'''i/p ->  
+4  quantity of friends Petya invited to the party
+1 2 3 4
+2 3 4 1 pi — the number of a friend who gave a gift to friend number i.
+o/p ->
+4 1 2 3 Print n space-separated integers:
+        the i-th number should equal the number of the friend who gave a gift to friend number i.
+'''
+
+class Solution:
+    def presents(self, n):
+        num = list(map(int, input().strip().split()))
+        dictt = {}
+        for i in range(1, n+1):
+            dictt[i] = num[i-1]
+        swapped_dict = {v: k for k, v in dictt.items()}
+        sorted_dict = {k: swapped_dict[k] for k in sorted(swapped_dict)}
+        new = list(sorted_dict.values())
+        new2 = " ".join(map(str, new))
+        
+        return new2
+
+n = int(input())
+sol = Solution()
+ans = sol.presents(n)
+print(ans)
+
+
+# dictt = {6:1, 1:2}
+# print(dictt.items())
+# print(dictt)
+# print(sorted(dictt))
+# sorted_dict = {k: dictt[k] for k in sorted(dictt)}
+# print(sorted_dict)
--- code_solutions/codeforces cp codes/tempCodeRunnerFile.py
+++ code_solutions/codeforces cp codes/tempCodeRunnerFile.py
@@ -1,18 +1,2 @@
-class Solution:
-    def Arrival_of_the_general(self, n):
-        num = list(map(int,input().split()))
-        big = float('-inf')
-        small = float('inf')
-        small_i = big_i = 0
-        for i in range(n):
-            if num[i]>big:
-                big = num[i]
-                big_i = i
-            if num[i]<=small:
-                small = num[i]
-                small_i = i
-        return (big_i) + (n-small_i-1)  
-
-n = int(input())
-sol = Solution()
-result = sol.Arrival_of_the_general(n)
+new = list(sorted_dict.values())
+new2 = "".join(new)
