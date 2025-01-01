21019bb45cd52d57bd798ea34220ba0096c9d1ca
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2025-01-01 11:31:49 +0530

    added selection_sort code file

---
+++ code_solutions/A2Z_dsa_code/selection_sort.py
@@ -0,0 +1,17 @@
+class Solution:
+    def selection_sort(self, arr):
+        n = len(arr)
+
+        for i in range(n):
+            min_index = i
+            for j in range(i+1, n):
+                if arr[j]<arr[min_index]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        return arr
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+sol = Solution()
+sorted_arr = sol.selection_sort(arr)
+print("Sorted Array:", sorted_arr)
