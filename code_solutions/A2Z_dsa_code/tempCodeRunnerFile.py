8eee83227bd97c03b335cd99d72de99a7723060a
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2025-01-08 13:18:34 +0530

    added new codefiles

--- code_solutions/A2Z_dsa_code/tempCodeRunnerFile.py
+++ code_solutions/A2Z_dsa_code/tempCodeRunnerFile.py
@@ -1,17 +1,39 @@
 class Solution:
-#     def selection_sort(self, arr):
-#         n = len(arr)
+#     def merge_sort(self, arr):
+#         # Base case: If the array has 1 or no elements, it's already sorted.
+#         if len(arr) <= 1:
+#             return arr
+        
+#         # Find the middle index to divide the array
+#         mid = len(arr) // 2
+#         # Recursively sort both halves
+#         l_half = self.merge_sort(arr[:mid])
+#         r_half = self.merge_sort(arr[mid:])
+        
+#         # Merge the sorted halves
+#         return self.merge(l_half, r_half)
 
-#         for i in range(n-1):
-#             min_index = i
-#             for j in range(i+1, n):
-#                 if arr[j]<arr[min_index]:
-#                     min_index = j
-#             arr[i], arr[min_index] = arr[min_index], arr[i]
-#         return arr
+#     def merge(self, left, right):
+#         new = []  # List to store the merged elements
+#         i, j = 0, 0
+        
+#         # Compare elements from both halves and merge them in sorted order
+#         while i < len(left) and j < len(right):
+#             if left[i] < right[j]:
+#                 new.append(left[i])
+#                 i += 1
+#             else:
+#                 new.append(right[j])
+#                 j += 1
+        
+#         # Add any remaining elements from the left or right half
+#         new.extend(left[i:])
+#         new.extend(right[j:])
+        
+#         return new
 
-
-# arr = [64, 34, 25, 12, 22, 11, 90]
+# # Example usage
+# arr = [4, 8, 5, 2, 6, 1, 66, 22, 35]
 # sol = Solution()
-# sorted_arr = sol.selection_sort(arr)
-# print("Sorted Array:", sorted_arr)
+# ans = sol.merge_sort(arr)
+# print(ans)
