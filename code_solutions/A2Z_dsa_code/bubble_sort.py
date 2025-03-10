0a1998d7aba16142291d5b07f91f48ac0f336fc0
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2025-03-10 14:43:57 +0530

    added bubble sort code with explanation

--- code_solutions/A2Z_dsa_code/bubble_sort.py
+++ code_solutions/A2Z_dsa_code/bubble_sort.py
@@ -15,17 +15,32 @@
 # sorted_array = bubble_sort(array)
 # print("Sorted array:", sorted_array)
 
+# class Solution:
+#     def bubble_sort1(self, arr):
+#         for i in range(len(arr)-1):
+#             swapped = False
+#             for j in range(len(arr)-i-1):
+#                 if arr[j]>arr[j+1]:
+#                     arr[j],arr[j+1] = arr[j+1], arr[j]
+#                     swapped = True
+#             if swapped == False:
+#                 return arr
+#         return arr
+
+
 class Solution:
-    def bubble_sort1(self, arr):
-        for i in range(len(arr)-1):
-            swapped = False
-            for j in range(len(arr)-i-1):
-                if arr[j]>arr[j+1]:
-                    arr[j],arr[j+1] = arr[j+1], arr[j]
-                    swapped = True
-            if swapped == False:
-                return arr
-        return arr
+    def bubbleSort(self, arr, n = None ):
+        
+        if n is None:
+            n = len(arr)
+        if n == 1:
+            return arr
+        for i in range(n-1):
+            if arr[i]>arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        
+        return self.bubbleSort(arr, n-1)
+    
 
 array = [64, 34, 25, 12, 22, 11, 90]
 sol = Solution()
