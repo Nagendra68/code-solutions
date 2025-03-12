38795a308c9c11e41458a3dddb106df60fbfb09e
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2025-03-12 12:00:34 +0530

    addded find max code

---
+++ code_solutions/A2Z_dsa_code/find max.py
@@ -0,0 +1,15 @@
+
+from typing import List
+
+
+class Solution:
+    def largest(self, arr):
+        # code here
+        n = len(arr)
+        maxx = arr[0]
+        for i in range(1,n):
+            if arr[i]> maxx:
+                maxx = arr[i]
+        return maxx
+        
+
